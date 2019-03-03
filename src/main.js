f8ebe4c1888d7e6cbc1dd054d342d6ad8b90348d
kedanhui <406852790@qq.com>
2019-03-03 09:15:20 +0800

    第一次上传

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+// 1.引入axios
+import axios from 'axios';
+
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+
+// 2.把axios绑定到vue实例的属性$axios
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
